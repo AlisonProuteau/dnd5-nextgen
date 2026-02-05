8334fda1ffb9567d13eb6b1376bb2d688c5e9e3b
“alison.prouteau” <alison.prouteau@workday.com>
2026-02-04 17:42:46 -0800

    Fix autofill

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -1,4 +1,4 @@
-import { type FormEvent, Fragment, useEffect, useState } from 'react';
+import { type SyntheticEvent, useEffect, useState } from 'react';
 import { Visibility, VisibilityOff } from '@mui/icons-material';
 import { Box, Button, Container, IconButton, InputAdornment } from '@mui/material';
 import { createUser, signIn } from '@api/users';
@@ -29,7 +29,7 @@ export function AuthPage() {
     form.updateValidationSchema(getLoginValidationSchema(isLogin));
   }, [isLogin]);
 
-  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (event: SyntheticEvent) => {
     event.preventDefault();
     if (isSaving) return;
 
@@ -50,73 +50,79 @@ export function AuthPage() {
         onBlur={({ target }) => target.id && form.validateField(target.id as keyof FormData)}
         onReset={() => form.resetForm()}
       >
-        {!isSaving ? (
-          <Fragment>
-            {!isLogin && (
-              <ControledInput
-                fullWidth
-                id="name"
-                type="text"
-                label="Display name"
-                onChange={(value) => form.setFormData({ name: value as string })}
-                errorMessage={form.getFieldError('name')}
-                hasError={!form.isFieldValid('name')}
-              />
-            )}
+        {!isLogin && (
+          <ControledInput
+            fullWidth
+            id="name"
+            type="text"
+            label="Display name"
+            autoComplete="username"
+            onInput={({ target }) =>
+              form.setFormData({ name: (target as HTMLInputElement).value as string })
+            }
+            errorMessage={form.getFieldError('name')}
+            hasError={!form.isFieldValid('name')}
+          />
+        )}
 
-            <ControledInput
-              fullWidth
-              id="email"
-              type="email"
-              label="Email address"
-              onChange={(value) => form.setFormData({ email: value as string })}
-              errorMessage={form.getFieldError('email')}
-              hasError={!form.isFieldValid('email')}
-            />
+        <ControledInput
+          fullWidth
+          id="email"
+          type="email"
+          label="Email address"
+          autoComplete="email"
+          onInput={({ target }) =>
+            form.setFormData({ email: (target as HTMLInputElement).value as string })
+          }
+          errorMessage={form.getFieldError('email')}
+          hasError={!form.isFieldValid('email')}
+        />
 
-            <ControledInput
-              fullWidth
-              id="password"
-              type={form.formData.showPassword ? 'text' : 'password'}
-              label="Password"
-              onChange={(value) => form.setFormData({ password: value as string })}
-              errorMessage={form.getFieldError('password')}
-              hasError={!form.isFieldValid('password')}
-              endAdornment={
-                <InputAdornment position="end">
-                  <IconButton
-                    onClick={() => form.setFormData({ showPassword: !form.formData.showPassword })}
-                    data-testid="password-visibility"
-                  >
-                    {form.formData.showPassword ? <VisibilityOff /> : <Visibility />}
-                  </IconButton>
-                </InputAdornment>
-              }
-            />
-            {!isLogin && (
-              <ControledInput
-                fullWidth
-                id="passwordConfrim"
-                type={form.formData.showPassword ? 'text' : 'password'}
-                label="Confirm Password"
-                onChange={(value) => form.setFormData({ passwordConfrim: value as string })}
-                errorMessage={form.getFieldError('passwordConfrim')}
-                hasError={!form.isFieldValid('passwordConfrim')}
-                endAdornment={
-                  <InputAdornment position="end">
-                    <IconButton
-                      onClick={() =>
-                        form.setFormData({ showPassword: !form.formData.showPassword })
-                      }
-                    >
-                      {form.formData.showPassword ? <VisibilityOff /> : <Visibility />}
-                    </IconButton>
-                  </InputAdornment>
-                }
-              />
-            )}
-          </Fragment>
-        ) : null}
+        <ControledInput
+          fullWidth
+          id="password"
+          type={form.formData.showPassword ? 'text' : 'password'}
+          label="Password"
+          autoComplete={isLogin ? 'current-password' : 'new-password'}
+          onInput={({ target }) =>
+            form.setFormData({ password: (target as HTMLInputElement).value as string })
+          }
+          errorMessage={form.getFieldError('password')}
+          hasError={!form.isFieldValid('password')}
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                onClick={() => form.setFormData({ showPassword: !form.formData.showPassword })}
+                data-testid="password-visibility"
+              >
+                {form.formData.showPassword ? <VisibilityOff /> : <Visibility />}
+              </IconButton>
+            </InputAdornment>
+          }
+        />
+
+        {!isLogin && (
+          <ControledInput
+            fullWidth
+            type={form.formData.showPassword ? 'text' : 'password'}
+            label="Confirm Password"
+            autoComplete="off"
+            onInput={({ target }) =>
+              form.setFormData({ passwordConfrim: (target as HTMLInputElement).value as string })
+            }
+            errorMessage={form.getFieldError('passwordConfrim')}
+            hasError={!form.isFieldValid('passwordConfrim')}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  onClick={() => form.setFormData({ showPassword: !form.formData.showPassword })}
+                >
+                  {form.formData.showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+        )}
 
         <Button
           disabled={!form.isValid || isSaving}
@@ -127,6 +133,7 @@ export function AuthPage() {
         >
           {isLogin ? 'Sign In' : 'Sign Up'}
         </Button>
+
         <Box display="flex" justifyContent="center" alignItems="center">
           {isLogin ? "Don't have an account?" : 'Already have an account?'}
           <Button type="reset" onClick={toggleLogin} disabled={isSaving}>
